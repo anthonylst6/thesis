cefda091df3dee05cb83a3b2af6f9c4a596ab927
anthonylst6 <anthony.liu@tutamail.com>
2022-10-01 06:10:54 +1000

    fixed global-glass-rolling typoes

--- scripts/global-glass-rolling.py
+++ scripts/global-glass-rolling.py
@@ -2,3 +2,4 @@ import importlib
 cf = importlib.import_module("calc_funcs_v1n")
 cf.priority = "memory"
 cf.calc_glass_rolling_avg_of_annual_diff("global", 1983, 2019, "all", 5, "modis")
+cf.calc_glass_rolling_avg_of_annual_diff("global", 1983, 2019, "all", 5, "avhrr")
